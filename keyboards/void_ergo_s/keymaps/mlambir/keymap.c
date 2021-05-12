495b8d63a047fddd6d8e893e3bdee7caf453560d
Mariano Lambir <mlambir@gmail.com>
2021-05-12 16:07:30 -0300

    update keymap

--- keyboards/void_ergo_s/keymaps/mlambir/keymap.c
+++ keyboards/void_ergo_s/keymaps/mlambir/keymap.c
@@ -29,34 +29,41 @@ enum user_layers {
 #define AL_DOWN LALT(KC_DOWN)
 #define AL_RGHT LALT(KC_RGHT)
 
+#define NUM_ENT LT(NUM_LAYER, KC_ENT)
+#define LOW_SPC LT(LOWER_LAYER, KC_SPC)
+#define RAI_SPC LT(RAISE_LAYER, KC_SPC)
+
+#define SFT_BSP LSFT_T(KC_BSPC)
+#define SFT_ENT RSFT_T(KC_ENT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE_LAYER] = LAYOUT_ortho_4x12(
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                        KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-  CTL_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                        KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  NUM,
+  CTL_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                        KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  NUM_ENT,
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_LPRN,  KC_RPRN,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                                KC_HYPR,  KC_LGUI,  LOWER,   KC_SPC,   KC_ENT,   RAISE,    KC_RGUI,  KC_RALT
+                                KC_HYPR,  KC_LGUI,  SFT_BSP, LOW_SPC,  RAI_SPC,  SFT_ENT,  KC_RGUI,  KC_RALT
 ),
 
 [LOWER_LAYER] = LAYOUT_ortho_4x12(
-  _______,  _______,  _______,  _______,  _______,  _______,                     _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  KC_EQL,                      _______,  _______,  _______,  _______,  KC_TILD,  _______,
-  _______,  KC_BSLS,  _______,  _______,  KC_UNDS,  KC_QUOT,  KC_LCBR,  KC_RCBR, KC_QUOT,  KC_UNDS,  _______,  _______,  KC_BSLS,  _______,
-                                _______,  _______,  _______,  _______,  _______, ADJUST,   _______,  _______
+  _______,  _______,  _______,  _______,  KC_LBRC,  KC_RBRC,                     _______,  _______,  _______,  _______,  _______,  _______,
+  _______,  _______,  _______,  KC_EQL,   KC_LPRN,  KC_RPRN,                     _______,  _______,  _______,  _______,  KC_TILD,  _______,
+  _______,  KC_BSLS,  _______,  _______,  KC_LCBR,  KC_RCBR,  KC_LCBR,  KC_RCBR, KC_QUOT,  KC_UNDS,  _______,  _______,  KC_BSLS,  _______,
+                                _______,  _______,  _______,  _______,  ADJUST,  _______,   _______,  _______
 ),
 
 [RAISE_LAYER] = LAYOUT_ortho_4x12(
   _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  _______,  KC_GRV,   KC_DEL,
-  _______,  _______,  _______,  _______,  _______,  KC_PLUS,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_COLN,  _______,
-  _______,  KC_PIPE,  _______,  _______,  KC_MINS,  KC_DQUO,  KC_LBRC,  KC_RBRC,  KC_DQUO,  KC_MINS,  _______,  _______,  KC_PIPE,  _______,
-                                _______,  _______,  ADJUST,   _______,  _______,  _______,  _______,  _______
+  _______,  _______,  _______,  KC_PLUS,  _______,  _______,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_COLN,  _______,
+  _______,  KC_PIPE,  _______,  _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_DQUO,  KC_MINS,  _______,  _______,  KC_PIPE,  _______,
+                                _______,  _______,  _______,  ADJUST,   _______,  _______,  _______,  _______
 ),
 
 [NUM_LAYER] = LAYOUT_ortho_4x12(
   _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                        KC_PSLS,  KC_4,     KC_5,     KC_6,     KC_PMNS,  _______,
   KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  KC_PAST,  KC_1,     KC_2,     KC_3,     KC_PPLS,  _______,
-                                _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_0
+                                _______,  _______,  _______,  _______,  _______,  _______,  KC_0,     KC_EQL
 ),
 
 [ADJUST_LAYER] = LAYOUT_ortho_4x12(
